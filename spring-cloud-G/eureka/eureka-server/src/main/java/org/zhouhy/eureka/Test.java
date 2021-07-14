aa1515a045b6bdee1c9cbcee405b1abeadeb137f
fengandzhy <fengandzhy@gmail.com>
2021-07-15 06:05:54 +1200

    spring-clould-G

---
+++ spring-cloud-G/eureka/eureka-server/src/main/java/org/zhouhy/eureka/Test.java
@@ -0,0 +1,7 @@
+package org.zhouhy.eureka;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello spring cloud!");
+    }
+}
--- spring-cloud/eureka/eureka-server/src/main/java/org/zhouhy/eureka/EurekaApp.java
+++ spring-cloud/eureka/eureka-server/src/main/java/org/zhouhy/eureka/EurekaApp.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApp {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
         SpringApplication.run(EurekaApp.class,args);
     }
 }
