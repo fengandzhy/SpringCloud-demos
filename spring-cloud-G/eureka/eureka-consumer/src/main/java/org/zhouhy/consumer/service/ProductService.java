be70e81e25724c7ce0c453ef815c624fc08f43f2
fengandzhy <fengandzhy@gmail.com>
2021-07-18 15:58:58 +1200

    eureka-consummer

---
+++ spring-cloud-G/eureka/eureka-consumer/src/main/java/org/zhouhy/consumer/service/ProductService.java
@@ -0,0 +1,39 @@
+package org.zhouhy.consumer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.zhouhy.consumer.domain.Product;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    
+    //http://192.168.78.151:8761/
+    private List<Product> listProduct(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka.provider");
+        StringBuffer sb = new StringBuffer();
+        sb.append("http://");
+        sb.append(serviceInstance.getHost());
+        sb.append(":");
+        sb.append(serviceInstance.getPort());
+        sb.append("/product/list");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ParameterizedTypeReference<List<Product>> typeReference 
+                = new ParameterizedTypeReference<List<Product>>(){};
+        ResponseEntity<List<Product>> responseEntity 
+                = restTemplate.exchange(sb.toString(), HttpMethod.GET,null,typeReference);
+        List<Product> products = responseEntity.getBody();               
+        return products;
+    }
+}
