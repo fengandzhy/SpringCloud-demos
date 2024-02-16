61db5a2193114766e8af5cd15a900170c54353c1
fengandzhy <fengandzhy@gmail.com>
2024-02-17 12:48:20 +1300

    application.yml

--- order_service/src/main/java/org/frank/spring/cloud/order/domains/Order.java
+++ order_service/src/main/java/org/frank/spring/cloud/order/domains/Order.java
@@ -1,4 +1,43 @@
 package org.frank.spring.cloud.order.domains;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private int orderId;
+    private String traceNo;
+    private User user;
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getTraceNo() {
+        return traceNo;
+    }
+
+    public void setTraceNo(String traceNo) {
+        this.traceNo = traceNo;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(List<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
 }
---
+++ order_service/src/main/java/org/frank/spring/cloud/order/services/impls/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package org.frank.spring.cloud.order.services.impls;
+
+import org.frank.spring.cloud.order.services.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
