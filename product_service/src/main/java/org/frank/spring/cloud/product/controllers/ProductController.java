64f660fcef7fb7d5c251bfd2713e9d196f7e3fc6
fengandzhy <fengandzhy@gmail.com>
2024-02-17 10:37:01 +1300

    product_service

---
+++ product_service/src/main/java/org/frank/spring/cloud/product/controllers/ProductController.java
@@ -0,0 +1,9 @@
+package org.frank.spring.cloud.product.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/product")
+public class ProductController {
+    
+    
+}
