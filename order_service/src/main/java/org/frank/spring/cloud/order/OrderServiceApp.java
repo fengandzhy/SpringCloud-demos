063aead3d4801d7b7d6403dfa79437842c0ca7af
fengandzhy <fengandzhy@gmail.com>
2024-02-17 16:18:30 +1300

    order_service

--- order_service/src/main/java/org/frank/spring/cloud/order/OrderServiceApp.java
+++ order_service/src/main/java/org/frank/spring/cloud/order/OrderServiceApp.java
@@ -3,9 +3,11 @@ package org.frank.spring.cloud.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @SpringBootApplication
 public class OrderServiceApp {
     public static void main(String[] args) {
