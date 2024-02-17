7fd687480b52e90ee8f593c0b2becaaafcc01b6f
fengandzhy <fengandzhy@gmail.com>
2024-02-17 13:37:27 +1300

    order_service

--- order_service/src/main/java/org/frank/spring/cloud/order/controllers/OrderController.java
+++ order_service/src/main/java/org/frank/spring/cloud/order/controllers/OrderController.java
@@ -2,6 +2,7 @@ package org.frank.spring.cloud.order.controllers;
 
 import org.frank.spring.cloud.order.domains.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,8 @@ import org.frank.spring.cloud.order.services.OrderService;
 public class OrderController {
     
     private OrderService orderService;
-    
+
+    @GetMapping("create")
     public Order createOrder(@RequestParam("user_id") Integer userId,
                              @RequestParam("prod_id") Integer prodId){
         return orderService.save(userId,prodId);
